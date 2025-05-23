502eb52104ba9ec8e4501ee55778660866ed81af
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2025-05-23 18:53:56 -0500

    extended inventory closing config

--- src/main/java/_3650/builders_inventory/feature/extended_inventory/ExtendedInventoryDeleteScreen.java
+++ src/main/java/_3650/builders_inventory/feature/extended_inventory/ExtendedInventoryDeleteScreen.java
@@ -12,6 +12,7 @@ import _3650.builders_inventory.ModKeybinds;
 import _3650.builders_inventory.api.util.GuiUtil;
 import _3650.builders_inventory.api.widgets.exbutton.ExtendedImageButton;
 import _3650.builders_inventory.api.widgets.exbutton.ExtendedImageButtonGui;
+import _3650.builders_inventory.config.Config;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.client.gui.ComponentPath;
@@ -159,6 +160,13 @@ public class ExtendedInventoryDeleteScreen extends Screen {
 			ExtendedInventory.open(this.minecraft);
 			return true;
 		} else return super.keyPressed(keyCode, scanCode, modifiers);
+	}
+	
+	@Override
+	public void onClose() {
+		if (Config.instance().extended_inventory_close_to_main) {
+			ExtendedInventory.open(this.minecraft);
+		} else super.onClose();
 	}
 	
 	@Override
