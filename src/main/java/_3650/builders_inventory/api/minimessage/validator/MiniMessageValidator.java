4b2e14d896c72378c5e273939fcb9d3c7c6ea1c3
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2026-01-24 03:05:03 -0600

    minimessage validation changes
    fixed chat minimessage validation forever and even yep uh huh yep
    - StringDiff is dead and I hope it stays gone
    moved highlighted minimessage reconstruction to validators
    - not everything is chat bro

--- src/main/java/_3650/builders_inventory/api/minimessage/validator/MiniMessageValidator.java
+++ src/main/java/_3650/builders_inventory/api/minimessage/validator/MiniMessageValidator.java
@@ -1,16 +1,41 @@
 package _3650.builders_inventory.api.minimessage.validator;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
+import _3650.builders_inventory.api.minimessage.instance.HighlightedTextInput;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 
 /**
- * Determines whether minimessage input should proceed and can modify the input text
+ * Determines whether minimessage input should proceed and can modify the input text<br>
+ * Also gets to fix any changes it made to the original text so the input displays properly :D
  */
 public interface MiniMessageValidator {
+	public static final MiniMessageValidator ALWAYS = new UnmodifiedMMValidator();
 	
-	public static final MiniMessageValidator ALWAYS = (minecraft, value) -> Optional.ofNullable(value);
+	public Optional<String> isValid(Minecraft minecraft, String value, Consumer<MiniMessageValidator> validatorChanger);
 	
-	public Optional<String> isValid(Minecraft minecraft, String value);
+	public void rebuildText(String original, String modified, MutableComponent highlighted, HighlightedTextInput.Builder output);
+	
+	/*
+	 * MiniMessage validator base type that doesn't modify the input in any way<br>
+	 * Not default in the interface because I don't want any validator that DOES modify the input to fail to implement rebuildText
+	 */
+	public static class UnmodifiedMMValidator implements MiniMessageValidator {
+		
+		@Override
+		public Optional<String> isValid(Minecraft minecraft, String value, Consumer<MiniMessageValidator> validatorChanger) {
+			return Optional.ofNullable(value);
+		}
+		
+		@Override
+		public void rebuildText(String original, String modified, MutableComponent highlighted, HighlightedTextInput.Builder output) {
+			output.visit(highlighted, Style.EMPTY);
+		}
+		
+	}
+	
 	
 }
