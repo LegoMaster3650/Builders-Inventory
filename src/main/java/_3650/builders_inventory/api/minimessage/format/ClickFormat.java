9bb98ca4193f35371b06282043cddcace13ad682
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2026-01-25 02:13:38 -0600

    made minimessage ClickFormat function similar to HoverFormat

--- src/main/java/_3650/builders_inventory/api/minimessage/format/ClickFormat.java
+++ src/main/java/_3650/builders_inventory/api/minimessage/format/ClickFormat.java
@@ -1,20 +1,111 @@
 package _3650.builders_inventory.api.minimessage.format;
 
+import java.net.URI;
+
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.MutableComponent;
 
 public class ClickFormat extends Format {
 	
-	public final ClickEvent event;
+	public final ClickContents<?> contents;
 	
-	public ClickFormat(String argString, String tag, ClickEvent event) {
+	public ClickFormat(String argString, String tag, ClickContents<?> contents) {
 		super(argString, tag);
-		this.event = event;
+		this.contents = contents;
 	}
 	
 	@Override
 	public MutableComponent format(MutableComponent component) {
-		return component.setStyle(component.getStyle().withClickEvent(event));
+		return component.setStyle(component.getStyle().withClickEvent(contents.build()));
+	}
+	
+	public static OpenUrlContents openUrl(URI uri) {
+		return new OpenUrlContents(uri);
+	}
+	
+	public static RunCommandContents runCommand(String command) {
+		return new RunCommandContents(command);
+	}
+	
+	public static SuggestCommandContents suggestCommand(String command) {
+		return new SuggestCommandContents(command);
+	}
+	
+	public static ChangePageContents changePage(int page) {
+		return new ChangePageContents(page);
+	}
+	
+	public static CopyToClipboardContents copyToClipboard(String value) {
+		return new CopyToClipboardContents(value);
+	}
+	
+	public static abstract class ClickContents<T extends ClickEvent> {
+		public final ClickEvent.Action type;
+		
+		protected ClickContents(ClickEvent.Action type) {
+			this.type = type;
+		}
+		protected abstract T build();
+	}
+	
+	private static class OpenUrlContents extends ClickContents<ClickEvent.OpenUrl> {
+		public final URI uri;
+		private OpenUrlContents(URI uri) {
+			super(ClickEvent.Action.OPEN_URL);
+			this.uri = uri;
+		}
+		@Override
+		protected ClickEvent.OpenUrl build() {
+			return new ClickEvent.OpenUrl(this.uri);
+		}
+	}
+	
+	private static class RunCommandContents extends ClickContents<ClickEvent.RunCommand> {
+		public final String command;
+		private RunCommandContents(String command) {
+			super(ClickEvent.Action.RUN_COMMAND);
+			this.command = command;
+		}
+		@Override
+		protected ClickEvent.RunCommand build() {
+			return new ClickEvent.RunCommand(this.command);
+		}
+	}
+	
+	private static class SuggestCommandContents extends ClickContents<ClickEvent.SuggestCommand> {
+		public final String command;
+		private SuggestCommandContents(String command) {
+			super(ClickEvent.Action.SUGGEST_COMMAND);
+			this.command = command;
+		}
+		@Override
+		protected ClickEvent.SuggestCommand build() {
+			return new ClickEvent.SuggestCommand(this.command);
+		}
+	}
+	
+	private static class ChangePageContents extends ClickContents<ClickEvent.ChangePage> {
+		public final int page;
+		private ChangePageContents(int page) {
+			super(ClickEvent.Action.CHANGE_PAGE);
+			this.page = page;
+		}
+		@Override
+		protected ClickEvent.ChangePage build() {
+			return new ClickEvent.ChangePage(this.page);
+		}
+	}
+	
+	private static class CopyToClipboardContents extends ClickContents<ClickEvent.CopyToClipboard> {
+		public final String value;
+		private CopyToClipboardContents(String value) {
+			super(ClickEvent.Action.COPY_TO_CLIPBOARD);
+			this.value = value;
+		}
+		@Override
+		protected ClickEvent.CopyToClipboard build() {
+			return new ClickEvent.CopyToClipboard(this.value);
+		}
 	}
 	
 }
