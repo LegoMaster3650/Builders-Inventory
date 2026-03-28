33755e02353459b35a75c01e1addb3c899ee38a8
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2026-03-28 01:03:46 -0500

    apparently isCreative may not tell the truth on some servers

--- src/main/java/_3650/builders_inventory/feature/hotbar_swapper/HotbarSwapper.java
+++ src/main/java/_3650/builders_inventory/feature/hotbar_swapper/HotbarSwapper.java
@@ -178,7 +178,7 @@ public class HotbarSwapper {
 	
 	public static void clientTick(Minecraft mc) {
 		if (mc.player == null) return;
-		if (!mc.player.isCreative()) {
+		if (!mc.player.hasInfiniteMaterials()) {
 			if (selecting) reset();
 			return;
 		}
@@ -253,7 +253,7 @@ public class HotbarSwapper {
 			reset();
 			return;
 		}
-		if (!mc.player.isCreative()) {
+		if (!mc.player.hasInfiniteMaterials()) {
 			reset();
 			return;
 		}
